0869b21d06ba667749696e32ecf5209e2dc041e6
EU\lehtpoll <olli-pekka.lehtonen@tieto.com>
2016-11-03 17:09:59 +0200

    AWS integration

---
+++ javasetup.bat
@@ -0,0 +1,3 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_102
+set PATH=%JAVA_HOME%\bin;%PATH%
--- src/main/java/com/gaming/ItsinthegameApplication.java
+++ src/main/java/com/gaming/ItsinthegameApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItsinthegameApplication {
+public class ItsinthegameApplication { //extends SpringBootServletInitializer {
 
+//	@Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(ItsinthegameApplication.class);
+//    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ItsinthegameApplication.class, args);
 	}
